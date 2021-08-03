9d4e628e7b1c2bbef672a60ed8fb2a909a24fe80
Daniil <daniil-murashov@yandex.ru>
2021-08-03 22:32:29 +0300

    add func

--- installReq.sh
+++ installReq.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-sudo wget https://github.com/moparisthebest/static-curl/releases/download/v7.78.0/curl-amd64 -O /usr/bin/curl
-sudo chmod +x /usr/bin/curl
+
+installDocker (){
+    if [[ -x "$(command -v curl)" ]]; then
+        echo "curl found"
+    else
+        echo "curl found"
+        sudo wget https://github.com/moparisthebest/static-curl/releases/download/v7.78.0/curl-amd64 -O /usr/bin/curl
+        sudo chmod +x /usr/bin/curl
+    fi
+}
+
 sudo curl -fsSL https://get.docker.com/ | sudo sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
